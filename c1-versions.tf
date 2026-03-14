142c234f734ad1d623c232e9436dcde7c811231d
Harman Singh <singh9765h@gmail.com>
2026-03-14 17:33:04 +0530

    backend set

--- c1-versions.tf
+++ c1-versions.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-stateless-prod"
+    key    = "network/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
